eb18017d21c07f487adfb46cbf33dbccbb1a740e
Eric Lanza <eric@lanza.io>
2019-05-20 10:13:50 -0600

    Initial Commit

---
+++ PlaylistC26/PlaylistC26/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistC26
+//
+//  Created by Eric Lanza on 5/20/19.
+//  Copyright © 2019 ETLanza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
